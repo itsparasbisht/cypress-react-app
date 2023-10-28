09815b91e1d9d319f34ff8bbcc5b5a84d0f193b5
itsparasbisht <iamparasbisht@gmail.com>
2023-10-28 14:30:32 +0530

    fundamentals test

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,5 +1,24 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("fundamentals test", () => {
+  it("contains correct header", () => {
+    cy.visit("/fundamentals");
+    cy.get('[data-test="fundamentals-header"]').should(
+      "contain.text",
+      "Testing Fundamentals"
+    );
+  });
+
+  it("accordian works correctly", () => {
+    cy.visit("/fundamentals");
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      "not.be.visible"
+    );
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click();
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      "be.visible"
+    );
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click();
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      "not.be.visible"
+    );
+  });
+});
