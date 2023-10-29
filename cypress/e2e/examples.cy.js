6b3ebb030ede2671e0d974436c93e5a8da921058
itsparasbisht <iamparasbisht@gmail.com>
2023-10-29 11:18:08 +0530

    testing list of items (its, within)

--- cypress/e2e/examples.cy.js
+++ cypress/e2e/examples.cy.js
@@ -14,11 +14,54 @@ describe("various examples", () => {
     cy.location("pathname").should("equal", "/fundamentals");
   });
 
-  it.only("intercepts", () => {
+  it("intercepts", () => {
     cy.intercept("POST", "http://localhost:3000/examples", {
       fixture: "example.json",
     });
 
     cy.getDataTest("post-button").click();
+  });
+
+  it.only("grudges", () => {
+    cy.contains(/add some grudges/i);
+
+    cy.getDataTest("grudge-list").within(() => {
+      cy.get("li").should("have.length", 0);
+    });
+
+    cy.getDataTest("clear-grudges-button").should("not.exist");
+
+    cy.getDataTest("grudge-input").within(() => {
+      cy.get("input").type("some grudge");
+    });
+    cy.getDataTest("add-grudge-button").click();
+    cy.getDataTest("grudge-list").within(() => {
+      cy.get("li").should("have.length", 1);
+    });
+
+    cy.getDataTest("grudge-input").within(() => {
+      cy.get("input").type("grudge 2");
+    });
+    cy.getDataTest("add-grudge-button").click();
+    cy.getDataTest("grudge-list").within(() => {
+      cy.get("li").should("have.length", 2);
+      cy.get("li").its(0).should("contain.text", "some grudge");
+    });
+
+    cy.getDataTest("grudge-list").within(() => {
+      cy.get("li")
+        .its(0)
+        .within(() => {
+          cy.get("button").click();
+        });
+    });
+    cy.getDataTest("grudge-list").within(() => {
+      cy.get("li").should("have.length", 1);
+    });
+
+    cy.getDataTest("clear-grudges-button").click();
+    cy.getDataTest("grudge-list").within(() => {
+      cy.get("li").should("have.length", 0);
+    });
   });
 });
